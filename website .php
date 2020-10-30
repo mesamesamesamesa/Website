5d8ffd0e91d75c8e39b74a3a067e35ed788673c2
mesamesamesamesa <73683412+mesamesamesamesa@users.noreply.github.com>
2020-10-30 12:54:15 +0700

    Add files via upload

---
+++ website .php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>63070139</title>
+    </head>
+    <body>
+        <h1>นายเมษา มานิตสกุลวงศ์ ชื่อเล่น พี</h1>
+        <p>My First Paragraph</p>
+        <img src="cat.jpg">
+        <img src="dog.jpg">
+        <br>
+        <a href="https://www.facebook.com/">I like social media</a>
+        
+        <h2>เรื่องตลก</h2>
+        <ul>
+            <li>ทดสอบ</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+    </body>
+</html>
